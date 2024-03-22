a325fd634ab123003c3d2321bc09aae375d4a774
MUHAMMAD SHAHZIL <164368568+MuhammadShahzilAI@users.noreply.github.com>
2024-03-22 16:03:14 +0500

    Q 10 ADDING NAME
    ASSIGMENT OF GOVERNER SINDH

---
+++ Program2.js
@@ -0,0 +1,6 @@
+//exrcise 10
+// My Name is M Shahzil
+// Dated: 20/02/2024
+//this Program Will Multiplication
+//this program will run simple code just like hello world
+console.log(5 * 2);
---
+++ app.js
@@ -0,0 +1,5 @@
+//exrcise 10
+// My Name is M Shahzil
+// Dated: 20/02/2024
+// this program will run simple code just like hello world
+console.log("Hello World is Simple Program");
